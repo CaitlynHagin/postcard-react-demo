b7d43dfca950ee5038124cae0fa14ba66080e4a6
CaitlynHagin <caitlyn32h@gmail.com>
2023-03-01 10:44:45 +0000

    inital commit
    followed card tutorial

---
+++ my-app/src/components/postcard.js
@@ -0,0 +1,38 @@
+//component name always has to be capitalised!! 
+
+
+// function Postcard() {
+//     return (
+//      <div class="Postcard"></div>
+
+//      <div className="card">
+//      <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" style={{width: '100%'}} />
+//      <div className="container">
+//        <h4><b>Jane Doe</b></h4>
+//        <p>Architect &amp; Engineer</p>
+//      </div>
+//    </div>
+
+//     );
+//   }
+  
+//   export default Postcard;
+  
+
+function Postcard() {
+    return (
+      <div>
+        <div class="Postcard"></div>
+  
+        <div className="card">
+          <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" style={{ width: '100%' }} />
+          <div className="container">
+            <h4><b>Jane Doe</b></h4>
+            <p>Architect &amp; Engineer</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Postcard;
